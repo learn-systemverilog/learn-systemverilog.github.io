c6b6936d71971047ff6f83fb38f43c418b84e448
Icaro-Lima <icarodantas.lima@gmail.com>
2021-02-08 21:35:56 -0300

    Better centering

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import Leds from './components/Leds';
 
 const styles = {
   overlay: {
+    width: '100wh',
     height: '100vh',
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    overflow: 'auto',
   },
+  inner: {
+    margin: 'auto',
+  }
 };
 
 function App() {
   return (
     <div style={styles.overlay}>
-      <Board background="#4285f4" border="#4b45cb">
-        <Board background="#ffc90b" border="#9a5f05">
-          <Switches />
+      <div style={styles.inner}>
+        <Board background="#4285f4" border="#4b45cb">
+          <Board background="#ffc90b" border="#9a5f05">
+            <Switches />
+          </Board>
+          <Leds />
+          <Board background="#3cdb83" border="#127141">
+            XX XXXX XX XXXXX
         </Board>
-        <Leds />
-        <Board background="#3cdb83" border="#127141">
-          XX XXXX XX XXXXX
         </Board>
-      </Board>
+      </div>
     </div>
   );
 }
