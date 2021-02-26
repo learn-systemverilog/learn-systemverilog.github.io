5ceb20431369e76031a47aaea6eb55fd0b2b05ca
Icaro-Lima <icarodantas.lima@gmail.com>
2021-02-25 21:34:34 -0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
-import { Stack, StackItem } from '@patternfly/react-core';
+import { Page, PageHeader, PageHeaderTools, PageSection, PageSectionVariants, Stack, StackItem } from '@patternfly/react-core';
 import { Bullseye, Card, CardBody, CardTitle } from '@patternfly/react-core';
 import Simulator from "./components/Simulator.js";
 import EditorCard from "./components/EditorCard.js";
 import ConsoleCard from "./components/ConsoleCard.js";
 
 const styles = {
-  padding: {
-    paddingTop: '5rem',
-    paddingBottom: '5rem',
-  },
   stack: {
     width: '70%',
   },
@@ -23,29 +19,37 @@ function App() {
     setLogs('');
   }
 
+  const Header = (
+    <PageHeader logo="Learn SystemVerilog" headerTools={<PageHeaderTools>header-tools</PageHeaderTools>} />
+  );
+
   return (
-    <Bullseye style={styles.padding}>
-      <Stack hasGutter style={styles.stack}>
-        <StackItem>
-          <Card>
-            <CardTitle>
-              Simulator
+    <Page header={Header}>
+      <PageSection variant={PageSectionVariants.default}>
+        <Bullseye>
+          <Stack hasGutter style={styles.stack}>
+            <StackItem>
+              <Card>
+                <CardTitle>
+                  Simulator
           </CardTitle>
-            <CardBody>
-              <Bullseye>
-                <Simulator code={transpiledCode} />
-              </Bullseye>
-            </CardBody>
-          </Card>
-        </StackItem>
-        <StackItem>
-          <EditorCard setLogs={setLogs} setTranspiledCode={setTranspiledCode} />
-        </StackItem>
-        <StackItem>
-          <ConsoleCard logs={logs} onClearLogs={clearLogs} />
-        </StackItem>
-      </Stack>
-    </Bullseye>
+                <CardBody>
+                  <Bullseye>
+                    <Simulator code={transpiledCode} />
+                  </Bullseye>
+                </CardBody>
+              </Card>
+            </StackItem>
+            <StackItem>
+              <EditorCard setLogs={setLogs} setTranspiledCode={setTranspiledCode} />
+            </StackItem>
+            <StackItem>
+              <ConsoleCard logs={logs} onClearLogs={clearLogs} />
+            </StackItem>
+          </Stack>
+        </Bullseye>
+      </PageSection>
+    </Page>
   );
 }
 
