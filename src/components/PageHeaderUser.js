3fb355cfec98391b77f5d91a08004053a614d612
Icaro-Lima <icarodantas.lima@gmail.com>
2021-03-07 16:50:41 +0000

    Remake User

--- src/components/PageHeaderUser.js
+++ src/components/PageHeaderUser.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { GoogleLogin, useGoogleLogout } from 'react-google-login';
-import { Bullseye, Split, SplitItem } from '@patternfly/react-core';
-import { PageHeaderToolsGroup, PageHeaderToolsItem } from '@patternfly/react-core';
-import { Avatar, Spinner, Button, Tooltip } from '@patternfly/react-core';
+import { useGoogleLogin, useGoogleLogout } from 'react-google-login';
+import { Dropdown, DropdownItem, DropdownToggle } from '@patternfly/react-core';
 
 export default function PageHeaderUser(props) {
     const user = props.user;
     const setUser = props.setUser;
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onToggle = (isOpen) => {
+        setIsOpen(isOpen);
+    };
 
     const onSignInSuccess = (response) => {
         setUser(user => ({
@@ -14,6 +18,7 @@ export default function PageHeaderUser(props) {
             isSignedIn: true,
             imageUrl: response.profileObj.imageUrl,
             name: response.profileObj.name,
+            givenName: response.profileObj.givenName,
             email: response.profileObj.email,
             tokenId: response.tokenId,
         }));
@@ -33,51 +38,49 @@ export default function PageHeaderUser(props) {
         setUser({ isSignedIn: false });
     }
 
+    const { signIn } = useGoogleLogin({
+        clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        buttonText: "Sign in with Google",
+        onSuccess: onSignInSuccess,
+        onAutoLoadFinished: onSignInAutoLoadFinished,
+        onFailure: onSignInFailure,
+        isSignedIn: true,
+        cookiePolicy: 'single_host_origin'
+    });
+
     const { signOut } = useGoogleLogout({
         clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
         onLogoutSuccess: onLogoutSuccess,
     });
 
-    const AvatarTooltipContent = (
-        <div>
-            Google Account<br />
-            {user.name}<br />
-            {user.email}<br />
-        </div>
+    const dropdownToggle = (
+        <DropdownToggle isDisabled={user.isSignedIn === undefined} onToggle={onToggle}>
+            {user.isSignedIn === true
+                ? user.email
+                : user.isSignedIn === false
+                    ? 'Sign in'
+                    : 'Loading...'
+            }
+        </DropdownToggle>
     );
 
+    const dropdownItemsSignedIn = [
+        <DropdownItem key="signout" onClick={signOut}>Sign out</DropdownItem>
+    ];
+
+    const dropdownItemsLoggedOut = [
+        <DropdownItem key="signin" onClick={signIn}>Sign in with Google</DropdownItem>
+    ];
+
+    const dropdownItems = () => {
+        if (user.isSignedIn) {
+            return dropdownItemsSignedIn;
+        }
+
+        return dropdownItemsLoggedOut;
+    };
+
     return (
-        <>
-            <PageHeaderToolsGroup visibility={{ default: user.isSignedIn !== false ? 'hidden' : 'visible' }}>
-                <PageHeaderToolsItem>
-                    <GoogleLogin
-                        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                        buttonText="Sign in with Google"
-                        onSuccess={onSignInSuccess}
-                        onAutoLoadFinished={onSignInAutoLoadFinished}
-                        onFailure={onSignInFailure}
-                        isSignedIn={true}
-                        cookiePolicy={'single_host_origin'}
-                    />
-                </PageHeaderToolsItem>
-            </PageHeaderToolsGroup>
-            {user.isSignedIn === true &&
-                <Split hasGutter>
-                    <SplitItem>
-                        <Bullseye>
-                            <Tooltip content={AvatarTooltipContent}>
-                                <Avatar src={user.imageUrl} alt="avatar" />
-                            </Tooltip>
-                        </Bullseye>
-                    </SplitItem>
-                    <SplitItem>
-                        <Button variant="secondary" onClick={signOut}>Log out</Button>
-                    </SplitItem>
-                </Split>
-            }
-            {user.isSignedIn === undefined &&
-                <Spinner size="lg" />
-            }
-        </>
+        <Dropdown isPlain isOpen={isOpen} position="right" toggle={dropdownToggle} dropdownItems={dropdownItems()} />
     );
 }
